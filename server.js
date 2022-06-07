5c30f6049206c022291b607e5da1d8b123d739b3
JuanEstuniga <JuanCZuniga@Juans-MacBook.local>
2022-06-07 09:55:58 -0400

    added login, and usernames

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,6 +17,36 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+var numberUsers = 0;
+io.on('connection', (socket) => {
+var userStarted = false;
+socket.on('new_message', (msg) => {
+socket.broadcast.emit('new_message', {
+username: socket.username,
+message: msg
+});
+});
+socket.on('user_added', (username) => {
+if (userStarted) return;
+socket.username = username;
+userStarted = true;
+numberUsers++;
+socket.emit('login', {
+numberOfUsers: numberUsers
+});
+socket.broadcast.emit('user_joined', {
+username: socket.username,
+numberOfUsers: numberUsers
+});
+});
+socket.on('disconnect', () => {
+if (userStarted) {
+--numberUsers;
+socket.broadcast.emit('user_left', {
+username: socket.username,
+numberOfUsers: numberUsers
+});
+
+}
+});
 });
